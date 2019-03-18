f0ce836c61ec9c77dcbd2a88d370dcde6a84d61e
Feng YangChing <stormy.petrel@msa.hinet.net>
2019-03-18 10:45:42 +0800

    graphqlsample  init

---
+++ GraphQLSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQLSample
+//
+//  Created by 馮仰靚 on 2019/3/18.
+//  Copyright © 2019 YCFeng.com.tw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ GraphQLSampleTests/GraphQLSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GraphQLSampleTests.swift
+//  GraphQLSampleTests
+//
+//  Created by 馮仰靚 on 2019/3/18.
+//  Copyright © 2019 YCFeng.com.tw. All rights reserved.
+//
+
+import XCTest
+@testable import GraphQLSample
+
+class GraphQLSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
